eaa05717b77acb8905674558e8c13d552e2e103b
DXZzZks <reyzeta88@gmail.com>
2024-09-19 22:46:14 -0600

    Actualizar .py

--- .py
+++ .py
@@ -1,3 +1,5 @@
-1 <IDOCTYPE html)  <html> charset="UTF-8" />  
-<meta name="viewport" content="width=device-width, initial-scale-1.0" />  <title>Interactive Street Lamp</title>  <link rel="stylesheet" href="style.css">  </head>  9  <body>  <input type="checkbox" name="btn" id="btn" checked  <div class="content">  <div class="buildings">  <div class="window"></div>  <div class="window"></div>  <div class="window"></div>  <div class="window">< div>  <div class="window"></div>  div class="window"></div>  </div>  <div class="ground">  div class="sewer"></div>  </div>  <div class="streetlamp"  <div class="base"></div>  <div class="basetop"></div>  <div class="pole"></div>  <div class="poletop"></div>  <div class="head">  <label for="btn"></label:  <div class="top"></div>  <div class="glass"></div>  <div class="bot"></div>  </div>  <div class="light"></div>  <div class="ground-light"></div>  </div>  </div>  </body>  </html>  
-3  4  5
+<!DOCTYPE html>
+ <html>
+   <head>
+     <meta charseta="UTF-8"/>
+<metaname="viewport"content=width=device -with,initial-scale=1.0"/>
